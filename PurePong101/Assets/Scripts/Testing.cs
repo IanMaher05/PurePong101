122e908fb4370035309fd411810ca8965090ccb7
IanMaher05 <113458615+IanMaher05@users.noreply.github.com>
2023-06-06 23:30:44 -0700

    Did some stuff for pure pong
    yep did that

---
+++ PurePong101/Assets/Scripts/Testing.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour {
+
+    private Prect p;
+    private Color c;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       p = new Prect("PrimRect", 40, 40, 100, 100, false, "simple");
+       c = new Color(1, 0.5f, 0);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnGUI()
+    {
+        Manager.GUIDrawRect(p.rect, c);
+    }
+
+}
